799fefc1688a2dd69f554c295d7fcddb11574d2b
David P <davidpdgt@gmail.com>
2026-01-05 15:55:07 +1000

    Added comments

--- api_requests.py
+++ api_requests.py
@@ -6,15 +6,43 @@ import json
 import requests
 
 from pathlib import Path
-from zoneinfo import ZoneInfo
 
 class adsb_api():
     def __init__(self, config_lat: float, config_lon: float, config_dist: float) -> None:
+        """
+        Initialise the class with the required variables. These variables are intented to be pulled from the config.json file
+        """
         self.config_latitude = config_lat
         self.config_longitude = config_lon
         self.config_distance = config_dist
 
+        if type(self.config_latitude) is not (float | int):
+            raise TypeError(f"The latitude is of the wrong type. Expecting float or int, recieved {type(self.config_latitude)}")
+        
+        if type(self.config_longitude) is not (float | int):
+            raise TypeError(f"The longitude is of the wrong type. Expecting float or int, recieved {type(self.config_longitude)}")
+        
+        if type(self.config_distance) is not (float | int):
+            raise TypeError(f"The distance is of the wrong type. Expecting float or int, recieved {type(self.config_distance)}")
+
+        if self.config_distance < 0:
+            raise ValueError(f"The distance value must be greater than 0. Recieved value {self.config_distance}")
+
     def get_data_from_api(self):
+        """
+        A function to request data from the API
+
+        Inputs:
+            None
+
+        Outputs:
+            aircraft_data: list
+                A list, where each entry is a json string
+            aircraft_number: int
+                Number of aircraft listed in aircraft_data
+            time_now: float
+                The time in seconds (Unix Epoch) the query was made
+        """
         construct_url = f"https://opendata.adsb.fi/api/v3/lat/{self.config_latitude}/lon/{self.config_longitude}/dist/{self.config_distance}"
 
         try:
@@ -29,6 +57,7 @@ class adsb_api():
         return aircraft_data, aircraft_number, time_now
 
 if __name__ == "__main__":
+    # Load directly from the config.json file if required
     parser = argparse.ArgumentParser()
     parser.add_argument('-j', '--json', type=str)
 
